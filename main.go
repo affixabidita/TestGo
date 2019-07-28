d4589445a13187e8b63eab713696e08a9b13cd1f
affixabidita <affixabidita@gmail.com>
2019-07-28 21:42:13 +0700

    try it

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var (
+	// flagPort is the open port the application listens on
+	flagPort = flag.String("port", "9000", "Port to listen on")
+)
+
+var results []string
+
+// GetHandler handles the index route
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	jsonBody, err := json.Marshal(results)
+	if err != nil {
+		http.Error(w, "Error converting results to json",
+			http.StatusInternalServerError)
+	}
+	w.Write(jsonBody)
+}
+
+// PostHandler converts post request body to string
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body",
+				http.StatusInternalServerError)
+		}
+		// results = append(results, string(body))
+		results = append(results, reverse(string(body)))
+		fmt.Fprint(w, "POST done")
+
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func init() {
+	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+	flag.Parse()
+}
+
+// func reverse(s []string) []string {
+// 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+// 		s[i], s[j] = s[j], s[i]
+// 	}
+// 	return s
+// }
+
+func reverse(s string) string {
+	chars := []rune(s)
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+	return string(chars)
+}
+
+func main() {
+	//results = append(results, time.Now().Format(time.RFC3339))
+
+	results = (results)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", GetHandler)
+	mux.HandleFunc("/post", PostHandler)
+
+	log.Printf("listening on port %s", *flagPort)
+	log.Fatal(http.ListenAndServe(":"+*flagPort, mux))
+}
